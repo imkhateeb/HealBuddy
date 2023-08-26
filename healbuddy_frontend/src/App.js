951aca016754ec734d6ad83d9b5329d1bf5aa809
imkhateeb <luckykhateeb2@gmail.com>
2023-08-26 12:41:01 +0530

    Pushed some test files

--- healbuddy_frontend/src/App.js
+++ healbuddy_frontend/src/App.js
@@ -8,6 +8,7 @@ import './CSS/customformstyle.css'
 import SetPassword from './component/SetPassword';
 
 import Test from './component/Test';
+import APITest from './component/APITest';
 
 const clientID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
@@ -20,6 +21,7 @@ function App() {
         <Route path='signup' element={<Signup />} />
         <Route path='test-page' element={<Test />} />
         <Route path='/*' element={<Home />} />
+        <Route path='/api-test' element={<APITest />} />
       </Routes>
     </GoogleOAuthProvider>
   );
---
+++ healbuddy_frontend/src/component/APITest.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function APITest() {
+  return (
+    <div>APITest</div>
+  )
+}
